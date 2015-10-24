164858bfb5ad52188656ac9724da72f007da40ed
Jayaraj Kubakaddi <jayaraj.sk@gmail.com>
2015-10-23 21:49:59 -0700

    Double linked list implementation and few operations that can be performed on a double linked list

---
+++ local_header.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define FALSE   0
+#define TRUE    1
+#define NO      0
+#define YES     1
+#define FAILURE 0
+#define SUCCESS 1
+
+typedef struct node_s
+{
+    int           num;
+    struct node_s *next;
+    struct node_s *prev;
+} node_t;
+
+
