0d71631ecb1a48215fffde7d1631a621fd9775f9
Jayaraj Kubakaddi <jayaraj.sk@gmail.com>
2015-11-22 16:00:17 -0800

    This program finds out the absolute of a given integer

---
+++ absolute.c
@@ -0,0 +1,20 @@
+/*
+ * This program finds out the absolute of a given integer
+ */
+#include <stdio.h>
+
+int
+main(int  argc,
+     char **argv)
+{
+    int          num;
+    unsigned int result;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+    int mask = num >> (sizeof(int) * (8 - 1));
+    result = (num + mask) ^ mask;
+
+    printf("Abs is %u\n", result);
+    return 0;
+}
