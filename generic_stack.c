01a6678fec4dbf528c59448ddf2d8c6ec8e8fc02
Jayaraj Kubakaddi <jayaraj.sk@gmail.com>
2015-12-07 21:00:42 -0800

    Improved readability

--- generic_stack.c
+++ generic_stack.c
@@ -10,79 +10,89 @@
 #include <stdbool.h>
 #include <string.h>
 
-typedef struct{
-    void *elems;           //pointer to array of elements
-    int elemSize;          //size of each element
-    int logLength;         //current num of elements in stack
-    int allocLength;       //max size of stack
-} stack;
-
-void StackNew(stack *s,
-              int   elemSize)
+typedef struct my_stack_s
 {
-    s->allocLength = 4;
-    s->elemSize = elemSize;
-    s->logLength = 0;
-    s->elems = malloc(elemSize * s->allocLength);
+    void *elems;              //pointer to array of elements
+    int elem_size;            //size of each element
+    int current_stack_elems;  //current num of elements in stack
+    int max_stack_size;       //max size of stack
+} my_stack_t;
+
+static void
+stack_init(my_stack_t *s,
+           int        elem_size)
+{
+    s->max_stack_size = 4;
+    s->elem_size = elem_size;
+    s->current_stack_elems = 0;
+    s->elems = malloc(elem_size * s->max_stack_size);
 
     assert(s->elems != NULL);
 }
 
-void StackDispose(stack *s)
+static void
+stack_destroy(my_stack_t *s)
 {
     free(s->elems);
 }
 
-void StackPush(stack      *s,
-               const void *elemAddr)
+static void
+stack_push(my_stack_t *s,
+           const void *elem_addr)
 {
     void *dest;
-    if (s->logLength == s->allocLength) {
-        s->allocLength *= 2;
-        s->elems = realloc(s->elems, s->elemSize * s->allocLength);
+    if (s->current_stack_elems == s->max_stack_size) {
+        s->max_stack_size *= 2;
+        s->elems = realloc(s->elems, s->elem_size * s->max_stack_size);
         assert(s->elems != NULL);
     }
 
-    dest = (char*)s->elems + ((s->logLength) * s->elemSize);
+    dest = (char*)s->elems + ((s->current_stack_elems) * s->elem_size);
 
-    memcpy(dest, elemAddr, s->elemSize);
-    s->logLength++;
+    memcpy(dest, elem_addr, s->elem_size);
+    s->current_stack_elems++;
 
 }
 
-bool StackEmpty(const stack *s)
+static bool
+is_stack_empty(const my_stack_t *s)
 {
-    return (s->logLength == 0);
+    return (s->current_stack_elems == 0);
 }
 
-void StackPop(stack *s,
-              void *elemAddr)
+static void
+stack_pop(my_stack_t *s,
+          void       *elem_addr)
 {
     void *src;
 
-    assert (!StackEmpty(s));
+    assert (!is_stack_empty(s));
 
+    s->current_stack_elems--;
+    src = (char*)s->elems + ((s->current_stack_elems) * s->elem_size);
 
-    s->logLength--;
-    src = (char*)s->elems + ((s->logLength) * s->elemSize);
-
-    memcpy(elemAddr, src, s->elemSize);
-
-
-
+    memcpy(elem_addr, src, s->elem_size);
 }
 
-int main(int argc, char *argv[])
+int
+main(int  argc,
+     char **argv)
 {
-    int val;
-    stack intStack;
-    StackNew(&intStack, sizeof(int));
-    for(val = 0; val < 6; val++)
-        StackPush(&intStack, &val);
-    while (!StackEmpty(&intStack)){
-        StackPop(&intStack, &val);
+    int        val;
+    my_stack_t int_stack;
+
+    stack_init(&int_stack, sizeof(int));
+
+    for(val = 0; val < 6; val++) {
+        stack_push(&int_stack, &val);
+    }
+
+    while (!is_stack_empty(&int_stack)) {
+        stack_pop(&int_stack, &val);
         printf("This just popped: %d\n", val);
     }
-    StackDispose(&intStack);
+
+    stack_destroy(&int_stack);
+
     return 0;
 }
